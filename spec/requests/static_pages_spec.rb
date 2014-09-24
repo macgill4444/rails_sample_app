c7704500ce402b07259a055a8f1f8c57774c9773
Macgill <macgill4444@gmail.com>
2014-09-24 11:42:42 -0400

    can't get contact page to load

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,7 +12,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Home")
     end
   end
 
@@ -26,7 +26,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("Help")
     end
   end
 
@@ -39,7 +39,15 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")  		
+      expect(page).to have_title("About Us")  		
   	end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact' 
+      expect(page).to have_content('Contact')
+    end
   end
 end
